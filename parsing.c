d2dac9054ec19d51d42ac58b57e8f05c0b8dfbde
Ahmad-Dorri <ahmaddorri110@gmail.com>
2026-02-04 10:14:52 +0330

    Add ^ operator

--- parsing.c
+++ parsing.c
@@ -1,4 +1,5 @@
 #include "mpc.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -76,6 +77,9 @@ long eval_op(long x, char *op, long y) {
   if (strcmp(op, "%") == 0) {
     return x % y;
   }
+  if (strcmp(op, "^") == 0) {
+    return pow(x, y);
+  }
   return 0;
 }
 
@@ -107,7 +111,7 @@ int main(int argc, char **argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
             "                                                     \
         number   : /-?[0-9]+/ ;                             \
-        operator : '+' | '-' | '*' | '/' | '%' ;                  \
+        operator : '+' | '-' | '*' | '/' | '%' | '^' ;                  \
         expr     : <number> | '(' <operator> <expr>+ ')' ;  \
         lispy    : /^/ <operator> <expr>+ /$/ ;             \
       ",
